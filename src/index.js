4f70e89ae5e3cb1f5d9762415f181fc20774ab27
Vitor Diniz <vitordiniz25@gmail.com>
2024-11-10 19:59:51 -0300

    chore: fileUpload setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -13,12 +15,22 @@ import statsRoutes from "./routes/stat.route.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-
 app.use(clerkMiddleware());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024, // 10MB
+    },
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
@@ -26,6 +38,19 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statsRoutes);
+
+// error handler
+
+app.use((err, req, res, next) => {
+  res
+    .status(500)
+    .json({
+      message:
+        process.env.NODE_ENV === "production"
+          ? "Internal server error"
+          : err.message,
+    });
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
