e9645b612e9224d22cd7fbbd25c6230db679682b
Vitor Diniz <vitordiniz25@gmail.com>
2024-11-10 20:00:41 -0300

    feat: create admin and album routes

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import {
+  createSong,
+  deleteSong,
+  createAlbum,
+  deleteAlbum,
+  checkAdmin,
+} from "../controller/admin.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Admin route with get method");
-});
+router.use(protectRoute, requireAdmin);
+
+router.get("/check", checkAdmin);
+
+router.post("/songs", createSong);
+router.delete("/songs/:id", deleteSong);
+
+router.post("/albums", createAlbum);
+router.delete("/albums/:id", deleteAlbum);
 
 export default router;
