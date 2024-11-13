d587f30bdc1e66a9b5a18835000716c3e966d5ea
Vitor Diniz <vitordiniz25@gmail.com>
2024-11-12 21:28:16 -0300

    feat: stat and user routes

--- src/routes/stat.route.js
+++ src/routes/stat.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { checkAdmin } from "../controller/admin.controller.js";
+
+import { getStatus } from "../controller/stat.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Stats route with get method");
-});
+router.get("/", protectRoute, checkAdmin, getStatus);
 
 export default router;
